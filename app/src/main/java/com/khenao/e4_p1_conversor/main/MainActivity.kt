191d530fabfce58f6e254c5533f1dd9cab2baed0
Katherin <katherin.henao@udea.edu.co>
2023-03-22 02:49:58 -0500

    se corrige y actualiza la act ppal

--- app/src/main/java/com/khenao/e4_p1_conversor/main/MainActivity.kt
+++ app/src/main/java/com/khenao/e4_p1_conversor/main/MainActivity.kt
@@ -11,8 +11,8 @@ import kotlin.concurrent.timerTask
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
     private val timer = Timer()
-    private val usd = 3944.4f
-    private val eur = 4133.47f
+    private val usd = 4813.4f
+    private val eur = 5181.47f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
